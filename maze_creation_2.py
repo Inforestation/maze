ed49e2c928b657599ac80241ff7bfd0eada1d185
Aleksandra Lipińska <a.t.lipinska@gmail.com>
2018-01-29 20:24:46 +0100

    Finished visualization of maze creation.

--- maze_creation_2.py
+++ maze_creation_2.py
@@ -107,18 +107,16 @@ def is_not_active(maze, x, y):
         return False
 
 
-def show_board(maze):
+def add_paths(maze, ax):
     x, y = zip(*(maze.stack))
-    plt.plot(x, y)
-    plt.ylim([-1, maze.dimension + 1])
-    plt.xlim([-1, maze.dimension + 1])
-    plt.show()
+    ax.plot(x, y, color='black', lw=1.5)
+    return ax
 
 
-def show_maze(maze):
+def add_maze(maze, ax):
     directions = [UP, RIGHT]
     coordinate_min = -0.5
-    coordinate_max = float(maze.dimension) - 0.5
+    coordinate_max = float(maze.dimension) + 0.5
     maze_walls = []
 
     for x in np.arange(coordinate_min, coordinate_max):
@@ -129,9 +127,40 @@ def show_maze(maze):
                 if coordinate_min < end_point[0] < coordinate_max and coordinate_min < end_point[1] < coordinate_max:
                     if is_not_in_path(start_point, maze, direction):
                         maze_walls.append((start_point, end_point))
-                    else:
-                        print end_point
-    print 'end'
+
+    maze_walls.append(((-0.5, maze.dimension - 0.5), (-0.5, -0.5)))
+    maze_walls.append(((-0.5, -0.5), (maze.dimension - 0.5, -0.5)))
+
+    for wall in maze_walls:
+        x = []
+        y = []
+        for point in wall:
+            x.append(point[0])
+            y.append(point[1])
+        ax.plot(x, y, color='k', lw=1.5)
+    return ax
+
+
+def show_all(maze):
+    figure, ax = plt.subplots(nrows=1)
+    add_maze(maze, ax)
+    add_start_and_stop(maze, ax)
+    #add_paths(maze, ax)
+    ax.set_ylim([-1.5, maze.dimension + 0.5])
+    ax.set_xlim([-1.5, maze.dimension + 0.5])
+    ax.set_aspect('equal')
+    ax.set_yticklabels([])
+    ax.set_xticklabels([])
+    ax.set_yticks([])
+    ax.set_xticks([])
+    plt.show()
+
+def add_start_and_stop(maze, ax):
+    start_point = (0, 0)
+    end_point = (maze.dimension - 1, maze.dimension - 1)
+    ax.scatter(start_point[0], start_point[1], color='b', lw=2)
+    ax.scatter(end_point[0], end_point[0], color='r', lw=2)
+    return ax
 
 
 def is_not_in_path(start_point, maze, direction):
@@ -157,5 +186,4 @@ def is_not_in_path(start_point, maze, direction):
 
 maze = Maze(DIMENSION)
 maze = move_forward(maze)
-show_board(maze)
-show_maze(maze)
+show_all(maze)
